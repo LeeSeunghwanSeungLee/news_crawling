c7caa895f0a730d20347e4744c0a6fd60bf84f70
lshn1007 <lshn1007@hanyang.ac.kr>
2021-08-11 16:04:15 +0900

    split keyword & add footnote

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# License : MIT
+# Project : News Crawling using specific keywords
+# Develop : Seunghwan Lee
+# Version : 1. 1. 2
+# Date    : 21-08-11
+
+ #-*- coding: utf-8 -*- 
 import requests
 import datetime
 import feedparser
@@ -7,16 +14,30 @@ import logging
 from goose3 import Goose
 from goose3.text import StopWordsKorean
 from gensim.summarization.summarizer import summarize
+import warnings
 
+# To Ignore warning sign
+warnings.filterwarnings('ignore')
+
+
+# Class :
 class newsScrap():
+    """Main Class related News Crawl"""
+
     def __init__(self): 
         print("Constructor")
         self._title = []
     def __del__(self): 
         print("Garbage Collection")
 
-    def exec(self, keyword, day, country, news_room):
-        print("Crawl")
+    """Get Webpage response method
+        @param keyword : keyword to look for news
+        @param day : how many days ago from today
+        @param country : 2 types ["ko" || "en"]
+        @param news_room : RSS feed list
+    """
+    def eccess(self, news_room):
+        print("Crawl start")
         URL = news_room # you need to override this method
         
         res = requests.get(URL)
@@ -29,6 +50,8 @@ class newsScrap():
         else:
             print("No response")
 
+
+    """Set data frame & change format & save (can override)"""
     def setDataFrame(self):
         raw_data = {'title' : self._title}
         res = pd.DataFrame(raw_data)
@@ -40,6 +63,14 @@ class newsScrap():
 
 
 class googleScrap(newsScrap):
+    """Extend NewsScrap class to use google news feed
+    @param _time : the time news writed
+    @param _link : news link
+    @param _summary : news context
+    @param _source : news source (ex. chosun, jungang...)
+    @param _keyword : title keyword(we are looking for)
+    @param _dataFrame : dataframe for changing format (.html, .csv...)
+    """
     def __init__(self):
         newsScrap.__init__(self)
         self._time = []
@@ -47,9 +78,10 @@ class googleScrap(newsScrap):
         self._summary = []
         self._source = []
         self._keyword = []
-        self._DataFrame = None
+        self._dataFrame = None
+        self._footNote = {}
 
-    def exec(self, keyword, day, country = 'ko'): # Google News Feed parsing method
+    def eccess(self, keyword, day, country = 'ko'): # Google News Feed parsing method
 
         print ('Google News Cron Start: ' + datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
         URL = 'https://news.google.com/rss/search?q={}+when:{}d'.format(keyword, day)
@@ -69,9 +101,9 @@ class googleScrap(newsScrap):
                 self._source.append(data.source.title)
                 self._keyword.append(keyword)   
                 self._link.append(data.link)
-                try:
+                try: # merge with company version
                     g = Goose({'stopwords_class':StopWordsKorean})
-                    article = g.extract(url=url)
+                    article = g.extract(url=URL)
                     self._summary.append(article.cleaned_text[:500])
                     # self._summary.append(article.meta_description)
                     # self._summary.append(summarize(article.cleaned_text[:1500]))
@@ -82,6 +114,15 @@ class googleScrap(newsScrap):
                                  
         else:
             print ('Google Search Error!!')
+
+    def addFootNote(self, keywords_li, country="kr"):
+        for keywords in keywords_li:
+            foot_link = "https://news.google.com/search?q={}".format(keywords)
+        if country == 'en':
+            foot_link += '&hl=en-NG&gl=NG&ceid=NG:en'
+        elif country == 'ko':
+            foot_link += '&hl=ko&gl=KR&ceid=KR:ko'
+        self._footNote.update({keywords : foot_link})
        
     def setDataFrame(self):
         raw_data = {
@@ -92,23 +133,31 @@ class googleScrap(newsScrap):
             'source' : self._source,
             'keyword' : self._keyword
         }
-        self._DataFrame = pd.DataFrame(raw_data)
+        self._dataFrame = pd.DataFrame(raw_data)
     
     def createCSV(self, file_name):
         file = './' + file_name + '.csv'
         if os.path.isfile(file):
             os.remove(file)
-        self._DataFrame.to_csv(file, encoding='utf-8-sig')
+        self._dataFrame.to_csv(file, encoding='utf-8-sig')
 
     def createHTML(self, file_name):
         file = './' + file_name + '.html'
         if os.path.isfile(file):
             os.remove(file)
-        self._DataFrame.to_html(file, encoding='utf-8-sig') # use (escape=False) if you want to make URL tag in html
+        self._dataFrame.to_html(file, encoding='utf-8-sig') # use (escape=False) if you want to make URL tag in html
 
-if __name__ == “__main__”
+    def appendFootNode(self, file_name):
+        # 마크업 링크를 만든다
+        markup = ""
+        # ./{file_name}.html에 추가로 입력한다.
+        with open("./{}.html".format(file_name), "a") as file:
+            file.write(markup)
+            file.close()
+        
+if __name__ == "__main__":
     today = googleScrap()
-    today.exec('스마트팩토리', 1)
+    today.eccess('smartfactory', 1)
     today.setDataFrame()
     today.createHTML('result')
     del today
